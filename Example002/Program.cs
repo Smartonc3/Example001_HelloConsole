ae3baf108c0dfb8b0a33a4275507e31bf5bb72fd
unknown <C:\Users\g3\AppData\Roaming\The Bat!>
2022-08-06 00:13:15 +0300

    Example2.1

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("22Hello, World!");
+﻿Console.Write("Введите ваше имя ");
+string username =  Console.ReadLine();
+Console.Write("Привет,  ");
+Console.Write(username);
+ 
