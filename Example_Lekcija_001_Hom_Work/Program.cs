7811ceea95b474d1164e19625bf4470d5a0a79ab
unknown <C:\Users\g3\AppData\Roaming\The Bat!>
2022-08-07 19:50:45 +0300

    Example_Lekcija_001_Hom_Work 1

---
+++ Example_Lekcija_001_Hom_Work/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт максимальное.
+
+Console.WriteLine("Введите Первое целое число, ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Второе целое число, ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Максимальное число = " + a );
+}
+else
+{
+    Console.WriteLine("Максимальное число = " + b);
+}
