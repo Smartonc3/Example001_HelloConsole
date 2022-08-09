9435dcca9bb1196aa356d525131bd59e0d3d94a7
unknown <C:\Users\g3\AppData\Roaming\The Bat!>
2022-08-09 21:58:32 +0300

    Задания с Семинара 2. C#.

---
+++ Example_seminar_C#_2/Program.cs
@@ -0,0 +1,82 @@
+﻿// int a = new Random().Next(10, 100);
+
+// Console.WriteLine("Случайное число о 10 до 99: " + a);
+
+// int FirstDigit = a  / 10;
+// int SecondDigit = a  % 10;
+
+
+// if (FirstDigit > SecondDigit)
+// {
+//      Console.WriteLine("Наибольшая цифра " + FirstDigit);
+    
+// }
+// else
+// {
+//      Console.WriteLine("Наибольшая цифра " + SecondDigit);
+// }
+
+
+
+//  Написать программу из 3 чисел которая удалает 2 число. из 3 значных делает 2 знвчное. 
+
+// int b = new Random().Next(100, 1000);
+
+// Console.WriteLine("Случайное число о 100 до 999: " + b);
+
+// int q = b / 100;   // Берём 1 значения от суммы
+// int w = b % 10;   // Берём последное значение из суммы. 
+
+// Console.WriteLine(q);
+// Console.WriteLine(w);
+// Console.WriteLine(q * 10 + w);
+
+
+// // Вводем 2 числа и проверяем число кратное или нет. Если нет то показать остатот от деления. 
+
+// Console.WriteLine("Введите Первое  число , ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Второе  число , ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a % b == 0)
+// {
+//     Console.WriteLine("Число кратное");
+// }
+// else
+// {
+//     Console.WriteLine( a % b );
+// }
+
+
+// Принимает число и решаем делется число на 7 и на 23 одновременно.
+
+
+// Console.WriteLine("Введите Первое  число , ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// if (a % 7 == 0 & a % 23 == 0)
+// {
+//     Console.WriteLine("да ");
+// }
+// else
+// {
+//     Console.WriteLine("нет ");
+// }
+
+// Программа которая принимает на вход два числа и проверяет, является ли одно число квадратом другого
+
+Console.WriteLine("Введите Первое  число , ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Второе  число , ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b || b * b == a)
+{
+    Console.WriteLine("да ");
+}
+else
+{
+    Console.WriteLine("нет ");
+}
+
