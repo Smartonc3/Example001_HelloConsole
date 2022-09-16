7a6bd43b21bb828a2eb5607e08b4f71fc52e8035
unknown <C:\Users\g3\AppData\Roaming\The Bat!>
2022-09-16 10:10:26 +0300

    щл

---
+++ SeminarC#_8/Program.cs
@@ -0,0 +1,220 @@
+﻿//  //  Задать 2 мерный массив. Написать программу которая поменает местами первую и последную страку. 
+
+
+// int m = Convert.ToInt32(new Random().Next(3, 5));
+// int n = m + Convert.ToInt32(new Random().Next(3, 5));
+
+// int[,] array = new int[m, n];
+
+// void FillAray(int[,] arr){
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(1, 10);
+//             if(arr[i, j] < 0) Console.Write(arr[i, j] + " ");
+//             else Console.Write(" " + arr[i, j] + " "); 
+//             // Console.Write(mart[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// void Swap(int[,] arr) 
+// {
+//     for (int j = 0; j < arr.GetLength(1) -1; j++)
+//     {
+//         int temp = 0; 
+//         temp = arr[0, j];
+//         arr[0, j] = arr[arr.GetLength(0) -1, j];
+//         arr[arr.GetLength(0) -1 , j] = temp;
+//     }
+// }
+
+// void Print(int[,] mart)                          
+// {
+//    for (int i = 0; i < mart.GetLength(0); i++)
+//     {
+//         for (int j = 0; j <mart.GetLength(1); j++)
+//         {
+//             if(mart[i, j] < 0) Console.Write(mart[i, j] + " ");
+//             else Console.Write(" " + mart[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// FillAray(array);
+// Swap(array);
+// Print(array);
+
+
+
+// // Задать двухмерный массив котора менает страку на столбцу. 
+// // 123        14
+// // 456        25
+// //            36
+
+
+// int m = Convert.ToInt32(new Random().Next(3, 5));
+// int n = m + Convert.ToInt32(new Random().Next(3, 5));
+
+// int[,] array = new int[m, n];
+
+// void FillAray(int[,] arr){
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(1, 10);
+//             if(arr[i, j] < 0) Console.Write(arr[i, j] + " ");
+//             else Console.Write(" " + arr[i, j] + " "); 
+//             // Console.Write(mart[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+
+// void Print(int[,] mart)                          
+// {
+//    for (int i = 0; i < mart.GetLength(0); i++)
+//     {
+//         for (int j = 0; j <mart.GetLength(1); j++)
+//         {
+//             if(mart[i, j] < 0) Console.Write(mart[i, j] + " ");
+//             else Console.Write(" " + mart[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int[,] Transverse(int[,] arr)
+// {
+//     int[,] arr1 = new int[n, m];
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++ )
+//         {
+//             arr1[j, i] = arr[i, j];
+//         }
+//     }
+//     return arr1;
+// }
+
+
+
+// FillAray(array);
+// Print(Transverse(array));
+// Console.WriteLine();
+// Print(array);
+
+
+// // Задать 2 мерный массив из 4 чисел. Напишите программу которая удалит страку и сталбец, на пересечении которых 
+// // располоден неименниий элимент массива.    
+                   // //  Наименьший элимент это 1 
+// // 1 4 7 2       9 2 3  
+// // 5 9 2 3       4 2 4
+// // 8 4 2 4       2 6 7
+// // 5 2 6 7
+
+
+
+int m = Convert.ToInt32(new Random().Next(4, 4));
+int n = Convert.ToInt32(new Random().Next(4, 4));
+
+int[,] array = new int[m, n];
+
+void FillAray(int[,] arr){
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+            if(arr[i, j] < 0) Console.Write(arr[i, j] + " ");
+            else Console.Write(" " + arr[i, j] + " "); 
+            // Console.Write(mart[i,j] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void Print(int[,] mart)                          
+{
+   for (int i = 0; i < mart.GetLength(0); i++)
+    {
+        for (int j = 0; j <mart.GetLength(1); j++)
+        {
+            if(mart[i, j] < 0) Console.Write(mart[i, j] + " ");
+            else Console.Write(" " + mart[i,j] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+FillAray(array);
+Console.WriteLine();
+
+void FindMinArray( int[,] arr, out int row, out int col)
+{
+    int min = arr [0,0];
+    row = 0;
+    col = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++ )
+        {
+            if (arr [i,j] < min) 
+            {
+                min = arr [i, j];
+                row = i;
+                col = j;
+            }
+        }
+    }
+}
+
+int x = 0, y = 0;
+FindMinArray(array, out x, out y);
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+
+int[,] Transverse(int[,] arr, int row, int col)
+{
+    int[,] arr1 = new int[arr.GetLength(0)-1, arr.GetLength(1)-1];
+    int shifti = 0;
+    int shiftj = 0;
+    for (int i = 0; i < arr.GetLength(0)-1; i++)
+    {
+        for (int j = 0; j < arr.GetLength(1)-1; j++ )
+        {
+            if (i < row) shifti =  0;
+            else shifti = 1;
+
+            if (j <  col) shiftj = 0; 
+            else shiftj = 1;
+            arr1[i, j] = arr[i + shifti, j + shiftj];
+        }
+    }
+    return arr1;
+}
+
+
+Print(Transverse(array, x, y)); 
+
+
---
+++ Seminar_C#_8_DZ/Program.cs
@@ -0,0 +1,176 @@
+﻿Console.Clear();
+
+
+// // // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// // // Например, задан массив:
+// // // 1 4 7 2
+// // // 5 9 2 3
+// // // 8 4 2 4
+// // // В итоге получается вот такой массив:
+// // // 7 4 2 1
+// // // 9 5 3 2
+// // // 8 4 4 2
+
+
+// int m = Convert.ToInt32(new Random().Next(3, 5));
+// int n = m + Convert.ToInt32(new Random().Next(3, 5));
+
+// int[,] array = new int[m, n];
+
+// void FillAray(int[,] arr){
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(1, 10);
+//             if(arr[i, j] < 0) Console.Write(arr[i, j] + " ");
+//             else Console.Write(" " + arr[i, j] + " "); 
+//             // Console.Write(mart[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+
+// void Print(int[,] mart)                          
+// {
+//    for (int i = 0; i < mart.GetLength(0); i++)
+//     {
+//         for (int j = 0; j <mart.GetLength(1); j++)
+//         {
+//             if(mart[i, j] < 0) Console.Write(mart[i, j] + " ");
+//             else Console.Write(" " + mart[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+
+// void Transverse(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       for (int k = 0; k < array.GetLength(1) - 1; k++)
+//       {
+//         if (array[i, k] < array[i, k + 1])
+//         {
+//           int temp = array[i, k + 1];
+//           array[i, k + 1] = array[i, k];
+//           array[i, k] = temp;
+//         }
+//       }
+//     }
+//   }
+// }
+
+
+
+// FillAray(array);
+// Transverse(array);
+// Console.WriteLine();
+// Print(array);
+
+
+
+
+
+// // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// // Например, задан массив:
+// // 1 4 7 2
+// // 5 9 2 3
+// // 8 4 2 4
+// // 5 2 6 7
+// // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+Console.Write("Введите Ширину и Длину массива 1 числом - ");
+int m = int.Parse(Console.ReadLine());
+int n = m;
+
+int[,] array = new int[m, n];
+
+void FillAray(int[,] mart){
+    for (int i = 0; i < mart.GetLength(0); i++)
+    {
+        for (int j = 0; j <mart.GetLength(1); j++)
+        {
+            mart[i, j] = new Random().Next(1, 10);
+            Console.Write(mart[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillAray(array);
+
+void SumEliment(int[,] mart){
+    int sum = 0;
+    int sumMin = 0;
+    for (int i = 0; i < mart.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j <mart.GetLength(1); j++)
+        {
+            sum = sum + mart[i, j];
+        }
+        Console.WriteLine(sum);
+        if (i == 0)
+        {
+            sumMin = sum;
+        }
+        else
+        {
+            if (sumMin < sum)
+            {
+                sumMin = sumMin;
+            }
+            else
+            {
+                sumMin = sum;
+            }
+        }
+        Console.WriteLine("Самая маленькая сумма - находится в страке " + i + " и состовлает " + sumMin);
+    } 
+     
+}
+
+SumEliment(array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void Print(int[,] mart)                            //   Метод печать. Но я изпользовал в 1 методе сразу. 
+// {
+//    for (int i = 0; i < mart.GetLength(0); i++)
+//     {
+//         for (int j = 0; j <mart.GetLength(1); j++)
+//         {
+//             Console.Write(mart[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// FillAray(array);
+// Print(array);
